6fb4b73a4068ec60ebc2942db7562ada65a05ce9
danielsana <danielsana60@gmail.com>
2024-06-13 12:10:49 +0300

    new

---
+++ main.js
@@ -0,0 +1,9 @@
+// arrays
+const people = ['mike','john','james']
+console.log(people)
+console.log(typeof(people))
+
+// objects
+const person = {firstName:"john",lastName:"smith",age:30}
+console.log(person)
+console.log(typeof(person))
---
+++ operators.js
@@ -0,0 +1,19 @@
+// arithmetic operaters
+// addition
+let num1 = 45
+let num2 = 50
+let num3 = num1+num2+50
+// console.log(num3)
+
+// subtraction 
+let num4 = num2 - num1 
+// console.log(num4)
+
+// multiplication
+let num5 = num1*num2 
+// console.log(num5) 
+
+// calculate the volume of the cylinder 
+// diameter = 144
+// pie 3.14
+// height 27
