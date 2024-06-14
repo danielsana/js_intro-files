b66f0e31da48586329ce05ac8955d4a744b5f523
danielsana <danielsana60@gmail.com>
2024-06-14 11:36:56 +0300

    lll

---
+++ functions.js
@@ -0,0 +1,8 @@
+function areaTriangle(){
+    let see_me = document.getElementById('area')
+    let base = 24;
+    let height = 12;
+    let area =1/2*base*height
+    see_me.innerHTML = area;
+}
+
